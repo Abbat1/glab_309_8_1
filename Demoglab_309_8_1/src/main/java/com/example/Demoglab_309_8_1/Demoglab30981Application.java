cab8cbf83b369cc393b7745c98db61a535122fc1
Abbat1 <cozzyyist@gmail.com>
2024-09-17 23:09:57 -0500

    glab_309_8_1

---
+++ Demoglab_309_8_1/src/main/java/com/example/Demoglab_309_8_1/Demoglab30981Application.java
@@ -0,0 +1,13 @@
+package com.example.Demoglab_309_8_1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Demoglab30981Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Demoglab30981Application.class, args);
+	}
+
+}
---
+++ Demoglab_309_8_1/src/test/java/com/example/Demoglab_309_8_1/Demoglab30981ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Demoglab_309_8_1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Demoglab30981ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
